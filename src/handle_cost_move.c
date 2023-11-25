c26032bc3b3641f35990474a57635efca7ded5f6
Renan Batista <contato@renanbatista.com.br>
2023-11-25 11:51:56 -0300

    ADD: search_next_position e search_last_position para tratar o custo de A, ainda com erro. ADD: handle_last_rotate para fazer os rotates finais e colocar a pilha em ordem correta. TODO: rotacao multipla em ambos os lados, para economizar movimentos, quando custo A e B sao positivos ou negativos.

--- src/handle_cost_move.c
+++ src/handle_cost_move.c
@@ -11,26 +11,67 @@ int	check_cost_move_b(int index_move, t_stack *stack_b)
 		return (index_move);
 }
 
+int	search_next_position(int target_position, t_stack *stack_a)
+{
+	int	index;
+	int	index_sub;
+	int	sun;
+	int	max_target_position;
+
+	max_target_position = -1;
+	index = -1;
+	max_target_position = stack_a->target_position_a[index];
+	while (++index, index <= stack_a->top_position)
+		if (stack_a->target_position_a[index] > max_target_position)
+			max_target_position = stack_a->target_position_a[index];
+	index = -1;
+	while (++index <= max_target_position)
+	{
+		index_sub = -1;
+		while (++index_sub, index_sub <= stack_a->top_position)
+		{
+			if (stack_a->target_position_a[index_sub] == target_position
+				+ index)
+				return (index_sub);
+		}
+	}
+	return (0);
+}
+
+int	search_last_position(int target_position, t_stack *stack_a)
+{
+	int	index_sub;
+
+	while (target_position)
+	{
+		index_sub = -1;
+		while (++index_sub, index_sub <= stack_a->top_position)
+		{
+			if (stack_a->target_position_a[index_sub] == target_position)
+				return (index_sub + 1);
+		}
+		target_position--;
+	}
+	return (0);
+}
+
 int	check_cost_move_a(int index_move, t_stack *stack_a, t_stack *stack_b)
 {
 	int	midlle_index;
-	int	index;
-	int	top_a_b;
+	int	next_position;
 
-	index = 0;
-	top_a_b = stack_a->top_position + stack_b->top_position + 1;
 	midlle_index = stack_a->top_position / 2;
-	while (index <= stack_a->top_position)
-	{
-		if (stack_a->target_position_a[index] > stack_b->target_position_a[index_move]
-			|| top_a_b == stack_b->target_position_a[index_move])
-			break ;
-		index++;
-	}
-	if (index <= midlle_index)
-		return (index);
+	if (stack_b->target_position_a[index_move] == stack_a->top_position
+		+ stack_b->top_position + 1)
+		next_position = search_last_position(stack_b->target_position_a[index_move],
+				stack_a);
 	else
-		return ((stack_a->top_position - (index + 1)) * -1);
+		next_position = search_next_position(stack_b->target_position_a[index_move],
+												stack_a);
+	if (next_position <= midlle_index)
+		return (next_position);
+	else
+		return ((stack_a->top_position - next_position + 1) * -1);
 }
 
 int	check_best_for_move(t_stack *stack_b)
@@ -40,7 +81,7 @@ int	check_best_for_move(t_stack *stack_b)
 	int	cost_move_minor;
 	int	swap_cost_move_minor;
 
-	index = 0;
+	index = -1;
 	cost_move_minor = __INT_MAX__;
 	while (++index <= stack_b->top_position)
 	{
--- src/handle_sort.c
+++ src/handle_sort.c
@@ -37,36 +37,68 @@ void	first_move_to_b(t_stack *stack_a, t_stack *stack_b)
 void	handle_move(int index, t_stack *stack_a, t_stack *stack_b)
 {
 	if (stack_b->cost_move_b[index] > 0)
-		while (stack_b->cost_move_b[index] > 0 && --stack_b->cost_move_b[index])
+		while (stack_b->cost_move_b[index] > 0 && stack_b->cost_move_b[index]--)
 			handle_operators("rb", stack_a, stack_b);
 	else
-		while (stack_b->cost_move_b[index] < 0 && ++stack_b->cost_move_b[index] != 0)
+		while (stack_b->cost_move_b[index] < 0 && stack_b->cost_move_b[index]++)
 			handle_operators("rrb", stack_a, stack_b);
 	if (stack_b->cost_move_a[index] > 0)
-		while (stack_b->cost_move_a[index] > 0 && --stack_b->cost_move_a[index])
+		while (stack_b->cost_move_a[index] > 0 && stack_b->cost_move_a[index]--)
 			handle_operators("ra", stack_a, stack_b);
 	else
-		while (stack_b->cost_move_a[index] < 0 && ++stack_b->cost_move_a[index] != 0)
+		while (stack_b->cost_move_a[index] < 0 && stack_b->cost_move_a[index]++)
 			handle_operators("rra", stack_a, stack_b);
 	handle_operators("pa", stack_a, stack_b);
-	
+}
+
+void	handle_last_rotate(t_stack *stack_a, t_stack *stack_b)
+{
+	int	index;
+	int	index_major_target_position;
+
+	index = -1;
+	index_major_target_position = 0;
+	while (++index <= stack_a->top_position)
+	{
+		if (stack_a->target_position_a[stack_a->top_position] == stack_a->target_position_a[index])
+			index_major_target_position = index;
+	}
+	if (index_major_target_position <= stack_a->top_position / 2)
+	{
+		while (stack_a->target_position_a[index_major_target_position] != index_major_target_position)
+		{
+			handle_operators("ra", stack_a, stack_b);
+			index_major_target_position--;
+		}
+	}
+	else
+	{
+		while (index_major_target_position != stack_a->top_position + 2)
+		{
+			handle_operators("rra", stack_a, stack_b);
+			index_major_target_position++;
+		}
+	}
 }
 
 int	handle_sort(t_stack *stack_a, t_stack *stack_b)
 {
-	int	index;
 	int	index_move;
+	// int	index;
+	int	index = 1;
 
 	if (check_is_order(stack_a, stack_b))
 		return (1);
 	first_move_to_b(stack_a, stack_b);
 	handle_order_three_values(stack_a, stack_b);
-	index = -1;
-	while (stack_b->top_position != -1)
+	// while (stack_b->top_position != -1)
+	while (index--)
 	{
 		index_move = check_index_move(stack_a, stack_b);
-		handle_move(index, stack_a, stack_b);
+		handle_move(index_move, stack_a, stack_b);
 	}
+		index_move = check_index_move(stack_a, stack_b);
+		handle_move(index_move, stack_a, stack_b);
+	// handle_last_rotate(stack_a, stack_b);
 	return (1);
 }
-// TODO: ao final preciso checar se nao preciso dar rotate ou inverse rotate para colocar o index 0 no começo
