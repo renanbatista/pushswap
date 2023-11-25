67aa5c963e2c42938e5446fbbf842ebbdb43f486
Renan Batista <contato@renanbatista.com.br>
2023-11-25 11:49:07 -0300

    fix: corrigido push e rotate_reverse, ambos estavam desconsiderando o target_position.

--- src/operators.c
+++ src/operators.c
@@ -6,7 +6,7 @@
 /*   By: r-afonso < r-afonso@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 20:24:24 by r-afonso          #+#    #+#             */
-/*   Updated: 2023/11/22 09:40:41 by r-afonso         ###   ########.fr       */
+/*   Updated: 2023/11/25 00:55:16 by r-afonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,18 @@ int	exec_swap(t_stack *stack)
 	return (0);
 }
 
-void	exec_push_a(t_stack *stack_one, t_stack *stack_two, int count)
+void	exec_push_continous(t_stack *stack_one, t_stack *stack_two, int index)
 {
-	count = ++stack_two->top_position + 1;
-	while (--count > 0)
+	stack_two->top_position++;
+	index = stack_two->top_position + 1;
+	while (--index > 0)
 	{
-		stack_two->numbers[count] = stack_two->numbers[count - 1];
-		stack_two->target_position_a[count] = stack_two->target_position_a[count
+		stack_two->numbers[index] = stack_two->numbers[index - 1];
+		stack_two->target_position_a[index] = stack_two->target_position_a[index
 			- 1];
 	}
-	stack_two->numbers[count] = stack_one->numbers[count];
-	stack_two->target_position_a[count] = stack_one->target_position_a[count];
+	stack_two->numbers[index] = stack_one->numbers[index];
+	stack_two->target_position_a[index] = stack_one->target_position_a[index];
 	stack_one->top_position--;
 }
 int	exec_push(t_stack *stack_one, t_stack *stack_two)
@@ -46,7 +47,7 @@ int	exec_push(t_stack *stack_one, t_stack *stack_two)
 	index = 0;
 	if (stack_one->top_position != -1)
 	{
-		exec_push_a(stack_one, stack_two, index);
+		exec_push_continous(stack_one, stack_two, index);
 		index = -1;
 		while (++index <= stack_one->top_position)
 		{
@@ -87,15 +88,21 @@ int	exec_rotate(t_stack *stack)
 int	exec_rotate_reverse(t_stack *stack)
 {
 	int	swap;
+	int	swap_targer_position;
 	int	count;
 
 	count = stack->top_position + 1;
 	swap = stack->numbers[stack->top_position];
+	swap_targer_position = stack->target_position_a[stack->top_position];
 	if (stack->top_position >= -1)
 	{
 		while (--count >= 0)
+		{
 			stack->numbers[count] = stack->numbers[count - 1];
+			stack->target_position_a[count] = stack->target_position_a[count - 1];
+		}
 		stack->numbers[0] = swap;
+		stack->target_position_a[0] = swap_targer_position;
 		return (1);
 	}
 	return (0);
