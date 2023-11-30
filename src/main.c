638a29bc07e06ae2c54ff66807d6956d3d2c07e8
Renan Afonso Batista <re.a.batista@gmail.com>
2023-11-30 13:01:57 -0300

    fix: retirado printf de teste e adicionado tratamento para argumentos menores que 2.

--- src/main.c
+++ src/main.c
@@ -6,16 +6,9 @@
 /*   By: r-afonso < r-afonso@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 12:54:02 by r-afonso          #+#    #+#             */
-/*   Updated: 2023/11/25 02:35:39 by r-afonso         ###   ########.fr       */
+/*   Updated: 2023/11/26 02:03:59 by r-afonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// TODO: validate params
-// TODO: move parans to stack_a
-// TODO: create alg for execute moviments
-// TODO: create moviments
-// TODO: validate performance
-// TODO: write tests.
 
 #include "../include/push_swap.h"
 
@@ -27,39 +20,22 @@ int	main(int argc, char **argv)
 
 	stack_a = start_stack(argc, argc - 2);
 	stack_b = start_stack(argc, -1);
-	stack_sub = ft_calloc(sizeof(int), argc - 2);
-	
-	if (!handle_validation(argc, argv, stack_a, stack_sub))
+	stack_sub = ft_calloc(sizeof(int), argc);
+	if (!handle_validation(argc, argv, stack_a, stack_sub) && stack_a)
 	{
-		ft_printf("Error\n");
-		return (1);
+		make_free(stack_a, stack_b, stack_sub);
+		if (argc != 1)
+			write(2, "Error\n", 6);
+		return (0);
 	}
 	if (!stack_a)
 		return (0);
 	search_target_pos(stack_a, stack_sub);
 	if (!handle_sort(stack_a, stack_b))
 	{
-		make_free(stack_a, stack_b);
+		make_free(stack_a, stack_b, stack_sub);
 		return (0);
 	}
-	ft_printf("\nStack A");
-	ft_printf(", Top: %d", stack_a->top_position);
-	for (int index = 0; index <= stack_a->top_position; index++)
-	{
-		ft_printf("\nIndex: %d", index);
-		ft_printf(", number: %d", stack_a->numbers[index]);
-		ft_printf(", targe_position: %d", stack_a->target_position_a[index]);
-	}
-	ft_printf("\n\nStack B");
-	ft_printf(", Top: %d", stack_b->top_position);
-	for (int index = 0; index <= stack_b->top_position; index++)
-	{
-		ft_printf(" \nIndex: %d", index);
-		ft_printf(", number: %d", stack_b->numbers[index]);
-		ft_printf(", targe_position: %d", stack_b->target_position_a[index]);
-		ft_printf(", cost A: %d", stack_b->cost_move_a[index]);
-		ft_printf(", cost B: %d", stack_b->cost_move_b[index]);
-	}
-	make_free(stack_a, stack_b);
+	make_free(stack_a, stack_b, stack_sub);
 	return (1);
 }
